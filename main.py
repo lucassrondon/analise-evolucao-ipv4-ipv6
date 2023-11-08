c962d2c6ee95d40aa3df771d87091ebc36e7e87c
lucassrondon <lucas.rondon@hotmail.com>
2023-11-08 17:25:47 -0300

    Melhorando formatacao dos dados

--- main.py
+++ main.py
@@ -12,22 +12,30 @@ for arquivo_nome in os.listdir(caminho_arquivos):
 
     with open(caminho_arquivo, "r") as arquivo:
         # setando/resetando variaveis de interesse
-        total_ipv4    = 0
-        total_ipv6    = 0
-        total_sa      = 0
-        ambos         = 0
-        somente_ipv4  = 0
-        somente_ipv6  = 0
+        total_anunciando_ipv4   = 0
+        total_anunciando_ipv6   = 0
+        total_sa                = 0
+        anunciando_ambos        = 0
+        anunciando_somente_ipv4 = 0
+        anunciando_somente_ipv6 = 0
+        quantidade_ipv4         = 0
+        quantidade_ipv6         = 0
 
         # listas de sistemas autonomos
         elementos_unicos = set()
-        elementos_ipv4   = set()
-        elementos_ipv6   = set()
+        sistemas_ipv4   = set()
+        sistemas_ipv6   = set()
+
+        # listas de ips
+        lista_de_ipv4 = set()
+        lista_de_ipv6 = set()
 
         for linha in arquivo:
             partes = linha.strip().split('|')
             
             if len(partes) >= 8:
+                """ Pegando e validando dados de uma linha """
+
                 ipv  = partes[1].strip()
                 rota = partes[2].strip()
                 
@@ -37,38 +45,52 @@ for arquivo_nome in os.listdir(caminho_arquivos):
                     continue
                 ultimo_elemento = elementos_rota[-1].strip()
 
+                """ Ifs para pegar as variaveis de interesse dos dados extraidos da linha """
+
                 # verifica se o sistema autonomo ainda não está na lista de sistemas autonomos
                 if ultimo_elemento not in elementos_unicos:
                     elementos_unicos.add(ultimo_elemento)
                     total_sa += 1
+                
+                # Pegando o total de elementos ipv4 e ipv6
+                if "." in ipv and ipv not in lista_de_ipv4:
+                    lista_de_ipv4.add(ipv)
+                    quantidade_ipv4 += 1
+                elif ":" in ipv and ipv not in lista_de_ipv6:
+                    lista_de_ipv6.add(ipv)
+                    quantidade_ipv6 += 1
                     
-                # Verifica se o IP é IPv4 ou IPv6 e se ainda nao está na lista
-                if "." in ipv and ultimo_elemento not in elementos_ipv4:
-                    elementos_ipv4.add(ultimo_elemento)
-                    total_ipv4 += 1
-                elif ":" in ipv and ultimo_elemento not in elementos_ipv6:
-                    elementos_ipv6.add(ultimo_elemento)
-                    total_ipv6 += 1
+                # Verifica se o IP é IPv4 ou IPv6 e se o AS que está 
+                # anunciando ainda nao está na lista
+                # Importante para pegar a quantidade de elementos unicos
+                if "." in ipv and ultimo_elemento not in sistemas_ipv4:
+                    sistemas_ipv4.add(ultimo_elemento)
+                    total_anunciando_ipv4 += 1
+                elif ":" in ipv and ultimo_elemento not in sistemas_ipv6:
+                    sistemas_ipv6.add(ultimo_elemento)
+                    total_anunciando_ipv6 += 1
 
-        # pegando quantidade de sistemas autonomos com ambos (ipv4 e ipv6) e com somente ipv4
-        for sistema_autonomo in elementos_ipv4:
-            if sistema_autonomo in elementos_ipv6:
-                ambos += 1
+        # pegando quantidade de sistemas autonomos com anunciando_ambos (ipv4 e ipv6) e com somente ipv4
+        for sistema_autonomo in sistemas_ipv4:
+            if sistema_autonomo in sistemas_ipv6:
+                anunciando_ambos += 1
             else:
-                somente_ipv4 += 1
+                anunciando_somente_ipv4 += 1
 
         # pegando quantidade de sistemas autonomos com somente ipv6
-        for sistema_autonomo in elementos_ipv6:
-            if sistema_autonomo not in elementos_ipv4:
-                somente_ipv6 += 1
+        for sistema_autonomo in sistemas_ipv6:
+            if sistema_autonomo not in sistemas_ipv4:
+                anunciando_somente_ipv6 += 1
         
         dados_por_data[arquivo_nome] = {
             'total_sa'     : total_sa,
-            'total_ipv4'   : total_ipv4,
-            'total_ipv6'   : total_ipv6,
-            'somente_ipv4' : somente_ipv4,
-            'somente_ipv6' : somente_ipv6,
-            'ambos'        : ambos
+            'quantidade_ipv4': quantidade_ipv4,
+            'quantidade_ipv6': quantidade_ipv6,
+            'total_anunciando_ipv4'   : total_anunciando_ipv4,
+            'total_anunciando_ipv6'   : total_anunciando_ipv6,
+            'anunciando_somente_ipv4' : anunciando_somente_ipv4,
+            'anunciando_somente_ipv6' : anunciando_somente_ipv6,
+            'anunciando_ambos'        : anunciando_ambos
         }
 
 # Imprime as contagens de cada arquivo
@@ -76,9 +98,11 @@ for chave in dados_por_data:
     print('---------------------------------------')
     print(f"Nome arquivo: {chave}")
     print(f"Total de ASes: {dados_por_data[chave]['total_sa']}")
-    print(f"Total de IPs IPv4: {dados_por_data[chave]['total_ipv4']}")
-    print(f"Total de IPs IPv6: {dados_por_data[chave]['total_ipv6']}")
-    print(f"Somente IPv4: {dados_por_data[chave]['somente_ipv4']}")
-    print(f"Somente IPv6: {dados_por_data[chave]['somente_ipv6']}")
-    print(f"Ambos: {dados_por_data[chave]['ambos']}")
+    print(f"Total de IPv4: {dados_por_data[chave]['quantidade_ipv4']}")
+    print(f"Total de IPv6: {dados_por_data[chave]['quantidade_ipv6']}")
+    print(f"Total ASes anunciando IPs IPv4: {dados_por_data[chave]['total_anunciando_ipv4']}")
+    print(f"Total ASes anunciando IPs IPv6: {dados_por_data[chave]['total_anunciando_ipv6']}")
+    print(f"ASes anunciando somente IPv4: {dados_por_data[chave]['anunciando_somente_ipv4']}")
+    print(f"ASes anunciando somente IPv6: {dados_por_data[chave]['anunciando_somente_ipv6']}")
+    print(f"Ambos: {dados_por_data[chave]['anunciando_ambos']}")
     print('---------------------------------------')
