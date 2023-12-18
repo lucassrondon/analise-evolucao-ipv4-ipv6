ccb97bff7a396acf52dd96886648478cdca009de
Gui <88752991+gvlk@users.noreply.github.com>
2023-12-18 17:51:17 -0300

    data now is stored in a file using pickle

--- main.py
+++ main.py
@@ -5,22 +5,40 @@
 
 from utils.parser import Parser
 from utils.graph_plotter import GraphPlotter
+from os.path import exists
+from pickle import load, dump
 
 RIB_DATA = "data_files/ribs/converted/"
-STATS_DATA = "data_files/stats"
+STATS_DATA = "data_files/stats/"
+CHARTS_DIR = "charts/"
+PICKLE_FILE_PATH = "data_files/parsed_data.pkl"
 
 if __name__ == '__main__':
-    print('Running...')
-    # Extraindo dados
-    parser = Parser(RIB_DATA, STATS_DATA)
-    dados = parser.run()
+    print("Running...")
+    if exists(PICKLE_FILE_PATH):
+        # Load data from the pickle file if it exists
+        print("Reading pickled data...")
+        with open(PICKLE_FILE_PATH, "rb") as pickle_file:
+            dados = load(pickle_file)
+    else:
+        # If the pickle file doesn't exist, run the parser to get the data
+        print("Parsing data...")
+        parser = Parser(RIB_DATA, STATS_DATA)
+        dados = parser.run()
+
+        # Save the parsed data to a pickle file
+        with open(PICKLE_FILE_PATH, "wb") as pickle_file:
+            dump(dados, pickle_file)
+        print(f"Data saved to pickle file at {PICKLE_FILE_PATH}")
 
     # Plotando gráficos
-    graph_plotter = GraphPlotter(dados=dados)
-
+    print("Plotting charts...")
+    graph_plotter = GraphPlotter(CHARTS_DIR, dados)
     graph_plotter.plot_grafico_total_sa()
     graph_plotter.plot_grafico_quantidade_ipv4()
     graph_plotter.plot_grafico_quantidade_ipv6()
+    graph_plotter.plot_grafico_ipv4_ipv6()
     graph_plotter.plot_grafico_sa_anunciando_ipv4()
     graph_plotter.plot_grafico_sa_anunciando_ipv6()
     graph_plotter.plot_grafico_sa_anunciando_ambos()
+    print("Done!")
